1f0c3d673adfad5d73643aa4921ff2c3698b475d
zxfjd3g <258147149@qq.com>
2020-12-22 11:51:48 +0800

    路由

---
+++ src/router/routes.js
@@ -0,0 +1,27 @@
+/* 
+所有路由匹配的数组
+*/
+import Home from '../pages/Home'
+import Search from '../pages/Search'
+import Register from '../pages/Register'
+import Login from '@/pages/Login'
+
+export default [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    name: 'search',
+    path: '/search/:keyword',
+    component: Search
+  },
+  {
+    path: '/register',
+    component: Register
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
