2d5cd97c4ef75bd4ea2478906429504d3f0805a9
zxfjd3g <258147149@qq.com>
2020-12-23 10:24:30 +0800

    axios二次封装

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="home">Home</div>
+  <div>
+    <TypeNav/>
+    <!-- <type-nav/> -->
+    <list-container/>
+    <TodayRecommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
+
+  </div>
 </template>
 
 <script>
+import ListContainer from './ListContainer'
+import TodayRecommend from './TodayRecommend'
+import Rank from './Rank/Rank.vue'
+import Like from './Like/Like.vue'
+import Floor from './Floor/Floor.vue'
+import Brand from './Brand/Brand.vue'
+
 export default {
   name: 'Home',
+
+  // 注册局部组件
+  components: {
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  }
 }
 </script>
-
-<style lang="less" scoped>
-  .home {
-    color: red;
-  }
-</style>
